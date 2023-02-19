689f22631c2f3db0a53bed87097c02910f41aaa8
Triz <kiebusiness1@gmail.com>
2023-02-19 21:26:18 +0000

    Added response for !join/!leave cmds.

--- src/commands/leave.ts
+++ src/commands/leave.ts
@@ -13,24 +13,14 @@ export default {
 
         for (const c of opts.args) {
             (await opts.getChatClient()).part(c)
+                .then(async c => {
+                    (await opts.getChatClient()).say(opts.channel, `@${opts.user} -> Leaving ${c}`);
+                })
                 .catch(async e => {
                     console.warn(`[Error] Failed to leave ${c}: ` + e);
                     (await opts.getChatClient()).say(opts.channel, 'Failed to leave channel: ' + c);
                 });
         }
-
-        // TODO: Fix `stored`.
-        const stored = (() => null)();
-        if (!stored) {
-            (await opts.getChatClient()).say(opts.channel, 'Failed to store channels.')
-        }
         return;
     }
 }
-
-// TODO: Change this to use internal sqlite db.
-// async function storeChannels(arr) {
-//     const qs = new URLSearchParams({ channels: arr.join('|') });
-//     const api = await fetch(`https://api.itsjusttriz.com/twitch/twitch-bot-channels?${qs}`, { method: 'DELETE' });
-//     return api.status === 200;
-// }
