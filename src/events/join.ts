354b5e150f7de26238b455b9b5938ae52ebba93e
Triz <kiebusiness1@gmail.com>
2023-02-19 18:32:30 +0000

    cleanup

--- src/events/join.ts
+++ src/events/join.ts
@@ -3,21 +3,13 @@ import { IJTTwitchClient } from "../utils/auth-provider";
 export default {
     name: 'join',
     once: false,
+
     run: async (channel: string, username: string, self: boolean, client: IJTTwitchClient) => {
         if (!self)
             return;
 
-        const chan_config = await getChannelConfig(channel).catch(e => {
-            console.warn(`[Error] Failed to run getChannelConfig(): ` + e);
-        });
-
-        if (chan_config)
-            (await client.getChatClient()).say('itsjusttriz', `Joined ${channel}`);
-        console.info('Joined', channel);
+        // (await client.getChatClient()).say('itsjusttriz', `Joined ${channel}`);
+        console.info('Joined', channel)
+        return;
     }
-}
-
-// TODO: Swap this for SQLite config.
-function getChannelConfig(channel: string) {
-    return new Promise((res, rej) => rej(`\n[event.join] Config for ${channel}, not found.`));
 }
