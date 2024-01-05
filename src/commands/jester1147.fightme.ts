da4d97b2c07663b0705e953775ca0efa42c55e49
itsjusttriz <kiebusiness1@gmail.com>
2024-01-05 01:18:52 +0000

    Added !fightme command

---
+++ src/commands/jester1147.fightme.ts
@@ -0,0 +1,24 @@
+import { Permissions } from '../utils/constants';
+import { Command } from '../utils/interfaces';
+import { IArray } from '@itsjusttriz/utils';
+
+export const command = {
+    name: 'fightme',
+    aliases: [],
+    permission: Permissions.REGULAR,
+    whitelisted_channels: ['ijtdev', 'jester1147'],
+    run: async (opts) => {
+        const replies = new IArray([
+            'Wanna put a bet on it?',
+            'Come and get some!',
+            'COME ON, THEN!',
+            "We both know, i'll win ;)",
+            "Aight, let's go...",
+            'KAPOW! Knock out time!',
+        ]);
+
+        const chosenReply = replies.getRandom();
+        await opts.chatClient.say(opts.channel, `@${opts.user} -> ${chosenReply}`);
+        return;
+    },
+} as Command;
