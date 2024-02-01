8d9c5370105d4caa2da4c579bfe39a339a538f91
itsjusttriz <kiebusiness1@gmail.com>
2024-02-01 02:23:37 +0000

    added uuid generator for future purposes with sql table manipulations

---
+++ src/helper/CreateUUID.ts
@@ -0,0 +1,8 @@
+export const createUUID = (length: number) => {
+    const digits = '0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += digits.charAt(Math.floor(Math.random() * digits.length));
+    }
+    return parseInt(result);
+};
