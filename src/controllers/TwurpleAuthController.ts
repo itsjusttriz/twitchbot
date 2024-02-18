0be4ac9d6330a036a64d5683bee889275a0c9777
itsjusttriz <kiebusiness1@gmail.com>
2024-02-18 23:28:05 +0000

    Updated @twurple

--- src/controllers/TwurpleAuthController.ts
+++ src/controllers/TwurpleAuthController.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { RefreshingAuthProvider } from '@twurple/auth';
 
 import { config } from './ClientConfigController';
+import { logger } from '../utils/Logger';
 
 async function createAuthProvider() {
     const tokenFilePath = path.resolve(__dirname, '../../tokens.json');
@@ -11,8 +12,11 @@ async function createAuthProvider() {
     const authProvider = new RefreshingAuthProvider({
         clientId: config?.TWITCH_CLIENT_ID,
         clientSecret: config?.TWITCH_CLIENT_SECRET,
-        onRefresh: async (userId, newTokenData) =>
-            await fsp.writeFile(tokenFilePath, JSON.stringify(newTokenData, null, 4), 'utf8'),
+    });
+
+    authProvider.onRefresh(async (userId, newTokenData) => {
+        logger.sysDebug.info('Refreshing tokens...');
+        await fsp.writeFile(tokenFilePath, JSON.stringify(newTokenData, null, 4), 'utf8');
     });
 
     authProvider.addUser('127667640', tokenData, ['chat']);
