c14d45ee580c9c0b2a5116d9006ca69ed2d0baed
itsjusttriz <kiebusiness1@gmail.com>
2024-02-19 00:54:23 +0000

    Add tmi userstate object

--- src/events/timeout.ts
+++ src/events/timeout.ts
@@ -1,16 +1,12 @@
+import { TimeoutUserstate } from 'tmi.js';
 import { logger } from '../utils/Logger';
 import { Event } from '../utils/interfaces/Event';
-
-type TimeoutTags = {
-    'target-user-id': string;
-    'room-id': string;
-};
 
 export const event = {
     name: 'timeout',
     once: false,
 
-    run: async (client, channel: string, username: string, reason: null, duration: number, tags: TimeoutTags) => {
+    run: async (client, channel: string, username: string, reason: null, duration: number, tags: TimeoutUserstate) => {
         logger.sysEvent.info(`TIMEOUT -> channel: ${channel} || username: ${username} || duration: ${duration}`);
         return;
     },
